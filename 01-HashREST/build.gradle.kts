de7a40298d3f4619ca47d362fe7c008b8e5edb5e
Stefan Penzinger <Stefan.Penzinger@fh-hagenberg.at>
2024-04-03 09:53:52 +0200

    EX-1 Add tasks to install and delete npm modules

--- 01-HashREST/build.gradle.kts
+++ 01-HashREST/build.gradle.kts
@@ -9,8 +9,10 @@ repositories {
     mavenCentral()
 }
 
+val commonsCodecVersion = "1.16.1"
+
 dependencies {
-    implementation("commons-codec:commons-codec:1.16.1")
+    implementation("commons-codec:commons-codec:$commonsCodecVersion")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
@@ -20,8 +22,28 @@ tasks.test {
     useJUnitPlatform()
 }
 
+tasks.register<Exec>("install-node-modules") {
+    group = "development"
+    description = "Installs the Node.js modules."
+    commandLine("npm", "install")
+}
+
 tasks.register<Exec>("start-hash-rest-server") {
     group = "development"
     description = "Starts the HashREST server."
+
+    dependsOn("install-node-modules")
     commandLine("node", "app.js")
+}
+
+tasks.register("cleanNodeModules") {
+    group = "development"
+    description = "Cleans the Node.js modules."
+    doLast {
+        project.delete("node_modules")
+    }
+}
+
+tasks.clean {
+    dependsOn("cleanNodeModules")
 }
