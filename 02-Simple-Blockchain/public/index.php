f43a798ef7f96e2d1dccf2a5979e0b2291039e10
Stefan Penzinger <Stefan.Penzinger@fh-hagenberg.at>
2024-04-19 13:55:40 +0200

    EX-2 Add state management of blockchains to be saved to a file

--- 02-Simple-Blockchain/public/index.php
+++ 02-Simple-Blockchain/public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use DI\Container;
 use Middleware\JsonMiddleware;
 use Models\BlockChain;
 use Psr\Http\Message\ResponseInterface as Response;
@@ -9,13 +10,46 @@ use Slim\Factory\AppFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+session_start();
+
+$SERVER_PORT = "SERVER_PORT";
+$BLOCKCHAIN_ID = "Blockchain" . $_SERVER[$SERVER_PORT];
+$NODE_IDENTIFIER = "NodeId" . $_SERVER[$SERVER_PORT];
+$BLOCKCHAIN_FILE_PATH = __DIR__ . '/../storage/' . $BLOCKCHAIN_ID;
+$NODE_IDENTIFIER_FILE_PATH = __DIR__ . '/../storage/' . $NODE_IDENTIFIER;
+
+$container = new Container();
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 $app->addMiddleware(new JsonMiddleware());
 
-$nodeIdentifier = str_replace('-', '', Uuid::uuid4()->toString());
-$blockchain = new BlockChain();
 
-$app->get('/mine', function (Request $request, Response $response) use ($blockchain, $nodeIdentifier) {
+if (file_exists($BLOCKCHAIN_FILE_PATH)) {
+    $serializedBlockchain = file_get_contents($BLOCKCHAIN_FILE_PATH);
+    $blockchain = unserialize($serializedBlockchain);
+} else {
+    $blockchain = new BlockChain();
+}
+
+$container->set($BLOCKCHAIN_ID, $blockchain);
+
+if (file_exists($NODE_IDENTIFIER_FILE_PATH)) {
+    $serializedNodeIdentifier = file_get_contents($NODE_IDENTIFIER_FILE_PATH);
+    $nodeIdentifier = unserialize($serializedNodeIdentifier);
+} else {
+    $nodeIdentifier = str_replace('-', '', Uuid::uuid4()->toString());
+    $serializedNodeIdentifier = serialize($nodeIdentifier);
+    file_put_contents($NODE_IDENTIFIER_FILE_PATH, $serializedNodeIdentifier);
+}
+
+$container->set($NODE_IDENTIFIER, $nodeIdentifier);
+
+$app->get('/mine', function (Request $request, Response $response)
+    use ($BLOCKCHAIN_ID, $NODE_IDENTIFIER, $BLOCKCHAIN_FILE_PATH) {
+    /** @var BlockChain $blockchain */
+    $blockchain = $this->get($BLOCKCHAIN_ID);
+    $nodeIdentifier = $this->get($NODE_IDENTIFIER);
+
     $lastBlock = $blockchain->lastBlock();
     $lastProof = $lastBlock['proof'];
     $proof = $blockchain->proofOfWork($lastProof);
@@ -31,11 +65,18 @@ $app->get('/mine', function (Request $request, Response $response) use ($blockch
         'proof' => $block['proof'],
         'previous_hash' => $block['previous_hash']
     ];
+
+    $serializedBlockchain = serialize($blockchain);
+    file_put_contents($BLOCKCHAIN_FILE_PATH, $serializedBlockchain);
+
     $response->getBody()->write(json_encode($body, JSON_PRETTY_PRINT));
     return $response;
 });
 
-$app->post('/transactions/new', function (Request $request, Response $response) use ($blockchain) {
+$app->post('/transactions/new', function (Request $request, Response $response)
+    use ($BLOCKCHAIN_ID, $BLOCKCHAIN_FILE_PATH) {
+    /** @var BlockChain $blockchain */
+    $blockchain = $this->get($BLOCKCHAIN_ID);
     $params = (array)$request->getParsedBody();
     $required = ['sender', 'recipient', 'amount'];
     $errors = array();
@@ -52,31 +93,42 @@ $app->post('/transactions/new', function (Request $request, Response $response) 
     }
 
     $index = $blockchain->newTransaction($params['sender'], $params['recipient'], $params['amount']);
-
     $body = ['message' => "Transaction will be added to block $index."];
+
+    $serializedBlockchain = serialize($blockchain);
+    file_put_contents($BLOCKCHAIN_FILE_PATH, $serializedBlockchain);
+
     $response->getBody()->write(json_encode($body, JSON_PRETTY_PRINT));
     return $response->withStatus(201);
 });
 
-$app->get('/chain', function (Request $request, Response $response) use ($blockchain) {
+$app->get('/chain', function (Request $request, Response $response) use ($BLOCKCHAIN_ID) {
+    /** @var BlockChain $blockchain */
+    $blockchain = $this->get($BLOCKCHAIN_ID);
     $body = ['chain' => $blockchain->chain(), 'length' => count($blockchain->chain())];
     $response->getBody()->write(json_encode($body, JSON_PRETTY_PRINT));
     return $response;
 });
 
-$app->post('/nodes/register', function (Request $request, Response $response) use ($blockchain) {
-    $params = (array)$request->getParsedBody();
-    $nodes = $params['nodes'] ?? [];
+$app->post('/nodes/register', function (Request $request, Response $response)
+    use ($BLOCKCHAIN_ID, $BLOCKCHAIN_FILE_PATH) {
+    /** @var BlockChain $blockchain */
+    $blockchain = $this->get($BLOCKCHAIN_ID);
+    $body = $request->getBody()->getContents();
+    $data = json_decode($body, true);
 
-    if (empty($nodes)) {
+    if (!isset($data['nodes'])) {
         $body = ['error' => 'Please supply a valid list of nodes'];
         $response->getBody()->write(json_encode($body, JSON_PRETTY_PRINT));
         return $response->withStatus(400);
     }
 
-    foreach ($nodes as $node) {
-        $blockchain->registerNode($node);
+    foreach ($data['nodes'] as $url) {
+        $blockchain->registerNode($url);
     }
+
+    $serializedBlockchain = serialize($blockchain);
+    file_put_contents($BLOCKCHAIN_FILE_PATH, $serializedBlockchain);
 
     $body = [
         'message' => 'New nodes have been added',
@@ -86,7 +138,10 @@ $app->post('/nodes/register', function (Request $request, Response $response) us
     return $response->withStatus(201);
 });
 
-$app->get('/nodes/resolve', function (Request $request, Response $response) use ($blockchain) {
+$app->get('/nodes/resolve', function (Request $request, Response $response)
+    use ($BLOCKCHAIN_ID, $BLOCKCHAIN_FILE_PATH) {
+    /** @var BlockChain $blockchain */
+    $blockchain = $this->get($BLOCKCHAIN_ID);
     $replaced = $blockchain->resolveConflicts();
 
     if ($replaced) {
@@ -94,6 +149,9 @@ $app->get('/nodes/resolve', function (Request $request, Response $response) use 
             'message' => 'Our chain was replaced',
             'new_chain' => $blockchain->chain()
         ];
+
+        $serializedBlockchain = serialize($blockchain);
+        file_put_contents($BLOCKCHAIN_FILE_PATH, $serializedBlockchain);
     } else {
         $body = [
             'message' => 'Our chain is authoritative',
